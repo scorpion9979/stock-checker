099eb9c8e9bb987bbed3d8409a274dd5d33b6f22
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2018-10-17 21:19:35 +0300

    Finished GET /api/stock-prices => stockData object for 1 stock

--- routes/api.js
+++ routes/api.js
@@ -18,11 +18,41 @@ module.exports = function(app) {
   const Schema = mongoose.Schema;
   const stockSchema = new Schema({
     ticker: {type: String, required: true},
-    likes: {type: Number, required: false, default: 0}
+    likes: {type: Number, required: false, default: 0},
   });
   const Stock = mongoose.model('Stock', stockSchema);
   app.route('/api/stock-prices')
      .get(function(req, res) {
-       
+       let ticker = req.query.stock;
+       Stock.find({ticker: ticker}, function(err, stocks) {
+        if (err) {
+          res.status(400)
+             .send(err);
+        } else if (stocks.length === 0) {
+          let s = new Stock({ticker: ticker});
+          s.save(function(e, doc) {
+            if (e) {
+              res.status(400)
+                 .send(err);
+            } else {
+              res.send({
+                stockData: {
+                  stock: doc.ticker,
+                  price: '0',
+                  likes: doc.likes,
+                },
+               });
+            }
+          });
+        } else {
+          res.send({
+            stockData: {
+              stock: stocks[0].ticker,
+              price: '0',
+              likes: stocks[0].likes,
+            },
+           });
+        }
+      });
      });
 };
